403b33e8b3e4947d1f156883d8de479a3ac51b33
Stardust <xiangbaobao2003@gmail.com>
2025-01-07 16:14:10 +0800

    fix asset path

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ import wasm from "vite-plugin-wasm";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
   css: {
     postcss: {
       plugins: [tailwind(), autoprefixer()],
